9353a812d683f59efcf73e46abcf0a1cb019d709
Kenneth Olivas <kennetholivas1@meltstudio.co>
2023-08-09 16:36:25 -0600

    feat: initial commit

---
+++ components/NavBar/index.tsx
@@ -0,0 +1,62 @@
+
+"use client";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, NavbarMenu, NavbarMenuToggle, NavbarMenuItem } from "@nextui-org/react";
+import { FC } from "react";
+
+
+const NavBarHeader: FC = () => {
+  const menuItems = [
+    "Home",
+    "About",
+  ];
+  return (
+    <Navbar isBlurred={false} className="bg-transparent" shouldHideOnScroll>
+      <NavbarContent>
+        <NavbarMenuToggle className="sm:hidden" />
+        <NavbarBrand>
+          <p className="font-bold text-inherit text-white">LOGO</p>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent className="hidden sm:flex gap-8" justify="start">
+        <NavbarItem>
+          <Link color="foreground" className="text-white" href="#">
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" className="text-white" href="#">
+            Services
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" className="text-white" href="#">
+            About
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" className="text-white" href="#">
+            Gallery
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Button as={Link} color="warning" href="#" variant="solid">
+            Contact Us
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link color="foreground" className="w-full" href="#" size="lg">
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+};
+
+export default NavBarHeader;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,52 @@
 import type { Config } from 'tailwindcss'
+const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          DEFAULT: "#3043B8",
+        },
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui({
+    themes: {
+      light: {
+        colors: {
+          background: "#FFFFFF", // or DEFAULT
+          foreground: "#11181C", // or 50 to 900 DEFAULT
+          primary: {
+            //... 50 to 900
+            foreground: "#FFFFFF",
+            DEFAULT: "##3043B8",
+          },
+          // ... rest of the colors
+        },
+      },
+    },
+    layout: {
+      disabledOpacity: "0.3", // opacity-[0.3]
+      radius: {
+        small: "2px", // rounded-small
+        medium: "4px", // rounded-medium
+        large: "6px", // rounded-large
+      },
+      borderWidth: {
+        small: "1px", // border-small
+        medium: "1px", // border-medium
+        large: "2px", // border-large
+      },
+    },
+  })]
 }
+
 export default config
