ce9b7cadfed9752b126e543bc72f11d1508084e5
Kenneth Olivas <kennetholivas1@meltstudio.co>
2023-08-24 16:34:11 -0600

    feat: add text mask animated

--- components/Hero/HeroImage.tsx
+++ components/Hero/HeroImage.tsx
@@ -5,17 +5,16 @@ import React from "react";
 
 const HeroImage = () => {
   return (
-    <div className="relative ml-4 col-span-6 hidden md:inline ">
+    <div>
       <NetxuiImage
         isBlurred
         as={Image}
-        width={700}
-        height={800}
-        className="w-full h-full rounded-md"
+        width={500}
+        height={700}
+        className="w-full  rounded-md"
         src="https://images.unsplash.com/photo-1665686377065-08ba896d16fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&h=800&q=80"
-        alt="Image Description"
+        alt="Hero Image"
       />
-      <div className="absolute inset-0 -z-[1] bg-gradient-to-tr from-primary via-white/0 to-secondary w-full h-full rounded-md mt-4 -mb-4 mr-4 -ml-4 lg:mt-6 lg:-mb-6 lg:mr-6 lg:-ml-6 "></div>
     </div>
   );
 };
