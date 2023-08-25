217cf2855fa7d653545812efbcc6094c2e75e44e
Kenneth Olivas <kennetholivas1@meltstudio.co>
2023-08-25 12:56:44 -0600

    feat: add work section

---
+++ components/WorkSection/index.tsx
@@ -0,0 +1,99 @@
+"use client";
+import { Button, Card, Link } from "@nextui-org/react";
+import { Image as NetxuiImage } from "@nextui-org/react";
+import Image from "next/image";
+
+const WorkSection = () => {
+  return (
+    <section className="w-full bg-primary rounded-lg md:py-24 mt-24">
+      <Card className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="grid gap-10 lg:grid-cols-2">
+          <div className="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-lg">
+            <div className="flex items-center justify-start w-16 h-16 mb-4 rounded-full bg-teal-accent-400">
+              <svg
+                className="text-teal-900 w-10 h-10"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                />
+              </svg>
+            </div>
+            <div className="max-w-xl mb-6">
+              <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+                View our work gallery
+              </h2>
+              <p className="text-base text-gray-700 md:text-lg">
+                lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor
+                sit amet, consectetur adip lorem ipsum dolor sit amet,
+                consectetur adip lorem ipsum dolor sit amet, consectetur adip
+                lorem ipsum
+              </p>
+            </div>
+            <div>
+              <Button
+                as={Link}
+                color="primary"
+                href="#"
+                variant="shadow"
+                endContent={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6">
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                    />
+                  </svg>
+                }>
+                Go to Gallery
+              </Button>
+            </div>
+          </div>
+          <div className="flex items-center justify-center -mx-4 lg:pl-8">
+            <div className="flex flex-col items-end px-3">
+              <NetxuiImage
+                as={Image}
+                width={200}
+                height={100}
+                className="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
+                src="https://images.unsplash.com/photo-1691514565027-ba527be3ec61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1917&q=80"
+                alt=""
+              />
+              <NetxuiImage
+                as={Image}
+                width={150}
+                height={50}
+                className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
+                src="https://images.unsplash.com/photo-1686058857415-8e6550648870?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
+                alt=""
+              />
+            </div>
+            <div className="px-3">
+              <NetxuiImage
+                as={Image}
+                width={300}
+                height={200}
+                className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
+                src="https://images.unsplash.com/photo-1600494603989-9650cf6ddd3d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </Card>
+    </section>
+  );
+};
+
+export default WorkSection;
