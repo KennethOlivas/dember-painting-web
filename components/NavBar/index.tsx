3a7829b6fee6ba56fdfa2dd31427a41fa40a34d3
Kenneth Olivas <kennetholivas1@meltstudio.co>
2023-08-29 13:30:48 -0600

    chore: ui navbar

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -10,61 +10,87 @@ import {
   NavbarMenuToggle,
   NavbarMenuItem,
 } from "@nextui-org/react";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+
+const menuItems = [
+  {
+    name: "Home",
+    href: "#",
+  },
+  {
+    name: "About",
+    href: "#about",
+  },
+  {
+    name: "Services",
+    href: "#services",
+  },
+  {
+    name: "Gallery",
+    href: "/gallery",
+  },
+];
 
 const NavBarHeader: FC = () => {
-  const menuItems = ["Home", "About"];
+  // create state for navbar if mobile true is not mobile false
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    // check if window width is less than 640px
+    if (window.innerWidth < 640) {
+      setIsMobile(true);
+    }
+    // add event listener to window resize
+    window.addEventListener("resize", () => {
+      // check if window width is less than 640px
+      if (window.innerWidth < 640) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    });
+  }, []);
+
   return (
-    <Navbar isBlurred={false} className="bg-transparent" position="static">
+    <Navbar
+      isBlurred={isMobile}
+      className={isMobile ? "" : "bg-transparent"}
+      position={isMobile ? "sticky" : "static"}>
       <NavbarContent>
-        <NavbarMenuToggle className="sm:hidden text-white" />
+        <NavbarMenuToggle className="sm:hidden md:text-white" />
         <NavbarBrand>
-          <p className="font-bold text-inherit text-white">LOGO</p>
+          <p className="font-bold text-inherit md:text-white">LOGO</p>
         </NavbarBrand>
       </NavbarContent>
       <NavbarContent className="hidden sm:flex gap-8" justify="start">
-        <NavbarItem>
-          <Link color="foreground" className="text-white" href="#">
-            Home
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" className="text-white" href="#services">
-            Services
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" className="text-white" href="#">
-            About
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" className="text-white" href="#">
-            Gallery
-          </Link>
-        </NavbarItem>
+        {menuItems.map((item, index) => (
+          <NavbarItem key={`${item.name}-${index}`}>
+            <Link color="foreground" className="text-white" href={item.href}>
+              {item.name}
+            </Link>
+          </NavbarItem>
+        ))}
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem>
           <Button
             as={Link}
             color="primary"
-            href="#"
+            href="#contact"
             variant="shadow"
             className="font-bold md:font-semibold">
             Contact Us
           </Button>
         </NavbarItem>
       </NavbarContent>
-      <NavbarMenu className="bg-opacity-80 bg-black">
+      <NavbarMenu className="bg-opacity-50 bg-black justify-center items-center space-y-8">
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
             <Link
               color="foreground"
-              className="w-full text-white"
-              href="#"
+              className="w-full text-white text-4xl text-center"
+              href={item.href}
               size="lg">
-              {item}
+              {item.name}
             </Link>
           </NavbarMenuItem>
         ))}
