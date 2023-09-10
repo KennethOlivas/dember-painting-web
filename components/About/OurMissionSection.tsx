f2727f56718676a6a48558fda80b785eb8218222
Kenneth Olivas <kennetholivas1@meltstudio.co>
2023-09-10 11:07:14 -0600

    feat: add mission animated

--- components/About/OurMissionSection.tsx
+++ components/About/OurMissionSection.tsx
@@ -1,21 +1,64 @@
 import { Card, CardBody, CardHeader } from '@nextui-org/react'
-import React from 'react'
+import React, { useEffect } from 'react'
 import OurMission from "@/constants/OurMission"
+import { motion } from 'framer-motion'
+import { useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+
+const container = {
+  hidden: { y: -100, scale: 0.5 },
+  visible: {
+    y: 0,
+    delay: 0.1,
+    scale: 1,
+    duration: 0.5,
+    transition: {
+      duration: 0.5,
+      delayChildren: 0.3,
+      staggerChildren: 0.2
+    }
+  }
+}
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1
+  }
+};
 
 const OurMissionSection = () => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
   return (
-    <div className="grid md:grid-cols-3 mt-12 text-white gap-4">
+    <motion.section
+      ref={ref}
+      animate={controls}
+      variants={container}
+      initial="hidden"
+      className="grid md:grid-cols-3 mt-12 text-white gap-4">
       {OurMission.map(({ content, title }, index) => (
-        <Card key={index}>
-          <CardHeader className="justify-center">
-            <h4 className="text-xl text-primary font-bold">{title}</h4>
-          </CardHeader>
-          <CardBody className="pt-0">
-            <p className='text-neutral-600'>{content}</p>
-          </CardBody>
-        </Card>
+        <motion.div key={index} variants={item}>
+          <Card className="h-full">
+            <CardHeader className="justify-center">
+              <h4 className="text-xl text-primary font-bold">{title}</h4>
+            </CardHeader>
+            <CardBody className="pt-0">
+              <p className='text-neutral-600'>{content}</p>
+            </CardBody>
+          </Card>
+        </motion.div>
       ))}
-    </div>
+    </motion.section>
   )
 }
 
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -37,7 +37,6 @@ const index = () => {
                   src="https://images.unsplash.com/photo-1692055651074-879fd6ab7909?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1965&q=80"
                 />
               </div>
-
             </div>
           </div>
         </div>
