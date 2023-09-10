adf5169e6ab06db4ccc9a0c0d9b1053ec6fb29ef
Kenneth Olivas <kennetholivas1@meltstudio.co>
2023-09-10 12:15:24 -0600

    feat: add animated text color hero

--- components/Hero/HeroHeader.tsx
+++ components/Hero/HeroHeader.tsx
@@ -10,7 +10,10 @@ const HeroHeader: FC = () => {
   return (
     <div className="flex flex-col justify-center  h-full">
        <h1 className="mb-4 text-4xl font-bold leading-snug text-dark sm:text-[42px] lg:text-[40px] xl:text-[42px]">
-        Transform Your Home into a Masterpiece of <span className="font-extrabold text-6xl tracking-wider text-transparent  bg-clip-text bg-gradient-to-r from-primary to-secondary">Color</span>
+        Transform Your Home into a Masterpiece of <span className="text-6xl font-extrabold 
+            bg-gradient-to-r bg-clip-text text-transparent tracking-wider
+            from-primary via-secondary to-danger
+            animate-text">Color</span>
       </h1>
       <p className="mb-8 max-w-[480px] text-xl text-inherit/30">
         Revitalize your home with our <span className="underline decoration-primary/70">painting</span> experts. Breathe new life into your spaces with us! Free consultation available.
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,23 @@ const config: Config = {
   ],
   darkMode: "class",
   theme: {
+    
     extend: {
+      animation: {
+        'text': 'text 5s ease infinite',
+      },
+      keyframes: {
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          }
+        },
+      },
       fontFamily: {
         sans: ['var(--font-cabin)'],
         mono: ['var(--font-roboto-mono)'],
