389c86c94b84a1903b6ecdf761563829c408df37
Kenneth Olivas <kennetholivas1@meltstudio.co>
2023-09-10 20:05:43 -0600

    fix: faq

--- constants/FAQList.ts
+++ constants/FAQList.ts
@@ -9,20 +9,20 @@ export const FAQList: FAQType[] = [
     answer: "There are several key things to look for when choosing a painting contractor. Check to see if they are licensed, insured, experienced and capable of completing the work.",
   },
   {
-    answer: "Can I leave my home while your painting crew works?",
-    question: "Yes. Most people we work for are not home while painting is done. We are licensed and fully insured.",
+    question: "Can I leave my home while your painting crew works?",
+    answer: "Yes. Most people we work for are not home while painting is done. We are licensed and fully insured.",
   },
   {
-    answer: "Do you have examples of work you've completed?",
     question: "Do you have examples of work you've completed?",
+    answer: "We sure do! Please visit our gallery page to view our work.",
   },
   {
-    answer: "Ok, I've decided to hire you. What's the next step?",
-    question: "First, we will send you a proposal and then schedule your job after approval. After that, we will reach out to you to schedule a meeting and go over the colors, type of paint, etc.",
+    question: "Ok, I've decided to hire you. What's the next step?",
+    answer: "First, we will send you a proposal and then schedule your job after approval. After that, we will reach out to you to schedule a meeting and go over the colors, type of paint, etc.",
   },
   {
-    answer: "Do you charge for estimates?",
-    question: "All of our estimates are free of charge.",
+    question: "Do you charge for estimates?",
+    answer: "All of our estimates are free of charge.",
   },
 ]
 
