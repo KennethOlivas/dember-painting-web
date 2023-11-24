05ec843f9f98aeee908b75c2c2f67878169b71c3
Kenneth Olivas <kennetholivas1@meltstudio.co>
2023-11-24 16:08:24 -0600

    feat: hide page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,26 @@
-import Hero from "@/components/Hero";
-import Services from "@/components/Services";
-import Faq from "@/components/Faq";
-import About from "@/components/About";
-import WorkSection from "@/components/WorkSection";
-import Contact from "@/components/Contact";
-import Testimonial from "@/components/Testimonial";
+// import Hero from "@/components/Hero";
+// import Services from "@/components/Services";
+// import Faq from "@/components/Faq";
+// import About from "@/components/About";
+// import WorkSection from "@/components/WorkSection";
+// import Contact from "@/components/Contact";
+// import Testimonial from "@/components/Testimonial";
 
 export default function Home() {
   return (
     <>
-      <Hero />
+      {/* Not found message */}
+      <div className="mt-24">
+        <h1 className="text-4xl text-center">404 - Page Not Found</h1>
+      </div>
+
+      {/* <Hero />
       <About />
       <Services />
       <WorkSection />
       <Testimonial />
       <Faq />
-      <Contact />
+      <Contact /> */}
     </>
   );
 }
